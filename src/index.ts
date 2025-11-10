ad717c36d8dfb077b22f022ded0c5bc9c0b689df
Andres Bravo <bravoandres1706@gmail.com>
2025-11-10 09:18:32 -0400

    remove dupicates from list solved

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { isPalindrome } from "./Exercises/PalindromeNumber";
 import RomanToInteger from "./Exercises/RomanToInteger";
+import removeDuplicates from "./Exercises/RemoveDuplicatesfromSortedArray";
 
 //console.log(isPalindrome(-121));
-console.log(RomanToInteger("III")); //58
+//console.log(RomanToInteger("III")); //58
 
-
+removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
